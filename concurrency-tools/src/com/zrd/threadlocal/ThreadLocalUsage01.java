000b446029414a722cf560e6b895c9d489ccca96
zhurundong <zhurundong@126.com>
2024-12-01 20:52:05 +0800

    ThreadLocalDemo

--- concurrency-tools/src/com/zrd/threadlocal/ThreadLocalUsage01.java
+++ concurrency-tools/src/com/zrd/threadlocal/ThreadLocalUsage01.java
@@ -14,20 +14,33 @@ import java.util.concurrent.Executors;
 public class ThreadLocalUsage01 {
     //线程池
     public static ExecutorService executorService = Executors.newFixedThreadPool(10);
+    //SimpleDateFormat 是线程不安全的类，一般不要定义为static变量
+    public static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
     public static void main(String[] args) {
         for(int i = 0 ; i < 20 ; i++){
             int finalI = i;
+            //executorService.submit(() -> System.out.println(unSafeDate(finalI)));
             executorService.submit(() -> System.out.println(new ThreadLocalUsage01().date(finalI)));
         }
         executorService.shutdown();
     }
 
+
+    public static String unSafeDate(int seconds){
+        Date date = new Date(1000L * seconds);
+        return simpleDateFormat.format(date);
+    }
+
     public String date(int seconds){
         //单位毫秒，从1970.1.1 00:00:00 GMT开始计时
-        Date date = new Date(1000L * seconds);
-        String format = ThreadSafeDateFormatter.dateFormatThreadLocal.get().format(date);
-        ThreadSafeDateFormatter.dateFormatThreadLocal.remove();
+        String format;
+        try {
+            Date date = new Date(1000L * seconds);
+            format = ThreadSafeDateFormatter.dateFormatThreadLocal.get().format(date);
+        } finally {
+            ThreadSafeDateFormatter.dateFormatThreadLocal.remove();
+        }
         return format;
     }
 
