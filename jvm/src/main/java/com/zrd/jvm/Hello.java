697d5c64d1fe9d5d34b2467b30a4d5c36d226671
zhurundong <zhurundong@126.com>
2024-12-21 19:53:21 +0800

    动态加载 class 文件

---
+++ jvm/src/main/java/com/zrd/jvm/Hello.java
@@ -0,0 +1,13 @@
+package com.zrd.jvm;
+
+/**
+ * @ClassName Hello
+ * @Description 动态加载 class 文件
+ * @Author ZRD
+ * @Date 2021/4/18
+ **/
+public class Hello {
+    static {
+        System.out.println("Hello Class Initialized!");
+    }
+}
